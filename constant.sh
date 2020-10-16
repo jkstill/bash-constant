2c35a19e2d3104a22e6da800b64fac730aaa2d65
Jared Still <jkstill@gmail.com>
2020-10-16 11:15:53 -0700

    initial

---
+++ constant.sh
@@ -0,0 +1,132 @@
+#!/usr/bin/env bash
+
+set -u
+
+#
+# 0 == off
+# 1 == on
+
+declare -A setopts
+
+# collect the starting values for 'set'
+# search for 'set \[' in 'man bash'
+collectSetDef() {
+
+	#initialize to off
+
+	for opt in a b e f h k m n p t u v x B C E H P T
+	do
+		setopts[$opt]=0
+	done
+	
+	local i=0
+	local currOpts=$-
+
+	while :
+	do
+		local opt=${currOpts:(( i++ )):1}
+		[[ -z $opt ]] && { break; }
+		setopts[$opt]=1
+	done
+
+}
+
+showSet() {
+
+	# bash 4.3.48 - seems to be sorting the keys
+	for setOpt in ${!setopts[@]}	
+	do
+		echo "set $setOpt: ${setopts[$setOpt]}"	
+	done
+
+}
+
+# reset a setting to the original value
+resetSet() {
+	local setOpt=$1
+
+	[[ -z $setOpt ]] && {
+		echo
+		echo "Invalid call witih empty parameter to resetSet()"
+		echo 
+		exit 1
+	}
+
+	# we need to make sure 'set -u' is not set here
+	set +u
+
+	local cmd='set -'
+	if [[ ${setopts[$setOpt]} -eq 0 ]]; then
+		eval "set +$setOpt"
+	else
+		eval "set -$setOpt"
+	fi
+
+	# set the +-u back to original
+	if [[ ${setopts[u]} -eq 0 ]]; then
+		eval "set +u"
+	else
+		eval "set -u"
+	fi
+
+}
+
+constant () {
+	set +u
+
+	declare -n var2set=$1
+
+	declare val2set="$2"
+	var2set="$val2set"
+	readonly var2set
+
+	# set +-u back to original
+	resetSet u
+}
+
+# may also work with ksh93+
+constantOldBash () {
+	set +u
+
+	# use printf -v if the bash is too old
+	declare var2set=$1
+	declare val2set="$2"
+	printf -v "$var2set" '%s' "$val2set"
+	readonly $var2set
+
+	# set +-u back to original
+	resetSet u
+}
+
+collectSetDef
+showSet
+
+cat <<-EOF
+
+  create a constant with the name 'myvar' and a value of 'myval'
+
+    constant myvar myval
+
+  This will be a readonly variable
+
+EOF
+
+constant myvar  myval
+#constantOldBash myvar  myval
+
+echo "myvar: $myvar"
+
+cat <<-EOF
+
+ The next command is 'myvar=changed'
+
+ This will fail, as myvar has been set to read only
+
+EOF
+
+myvar='changed'
+
+echo "myvar: $myvar"
+
+
+
